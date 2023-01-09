a8f76af7f4d426520cbe4415ad7f58473f77ee55
Mai Grand <mkmaxz48@gmail.com>
2023-01-09 12:38:31 +0300

    fix: очистка контекста в тестах

--- src/test/java/com/maigrand/overwatchdb/AbstractTest.java
+++ src/test/java/com/maigrand/overwatchdb/AbstractTest.java
@@ -3,10 +3,13 @@ package com.maigrand.overwatchdb;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
 @TestInstance(Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
 public abstract class AbstractTest {
 }
