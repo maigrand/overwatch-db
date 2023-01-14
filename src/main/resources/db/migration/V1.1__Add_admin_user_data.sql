fbc4c07f0750b743f8ca985ffc1640a31059a37b
Mai Grand <mkmaxz48@gmail.com>
2023-01-14 16:37:42 +0300

    fix: test user data

--- src/main/resources/db/migration/V1.1__Add_admin_user_data.sql
+++ src/main/resources/db/migration/V1.1__Add_admin_user_data.sql
@@ -1,2 +1,2 @@
 INSERT INTO admin_user (active, email, password)
-VALUES ('true', 'test@gmail.com', '30rOX0EYHFKDa26I');
+VALUES ('true', 'test@test.com', '$2a$10$SM1ERfi69u5uzJURXZ9csulgIFWMlUYR2Ru1RAggnzewGnoC0.zUu');
